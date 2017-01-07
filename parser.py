bbc49a7b0d8ea3dbf4a0f0761abcac0a8f2c1bdd
Laura Goon <laura.gooon@gmail.com>
2017-01-07 16:36:20 -0500

    outputs most common occurence for first+last name verified for rec

--- parser.py
+++ parser.py
@@ -1,5 +1,9 @@
 import requests
 import json
+import itertools
+import operator
+from nltk.tag.stanford import StanfordNERTagger
+st = StanfordNERTagger('stanford-ner/english.all.3class.distsim.crf.ser.gz', 'stanford-ner/stanford-ner.jar')
 
 def ocr_space_file(filename, overlay=False, api_key='186247b66c88957', language='eng'):
     """ OCR.space API request with local file.
@@ -58,5 +62,49 @@ test_url = ocr_space_url(url='http://www.pages.drexel.edu/~et95/final/images/Res
 rec_json = json.loads(test_file)
 resume_json = json.loads(test_url)
 
-print(rec_json['ParsedResults'][0]['ParsedText'])
-print(resume_json['ParsedResults'][0]['ParsedText'])
+# print(rec_json['ParsedResults'][0]['ParsedText'])
+# print(resume_json['ParsedResults'][0]['ParsedText'])
+
+
+
+# look for name
+
+#--- remove newlines
+recj = rec_json['ParsedResults'][0]['ParsedText']
+# recj = recj.replace("\r","")
+# recj = recj.replace("\n","")
+recj = recj.splitlines()
+# print(recj)
+resumej = resume_json['ParsedResults'][0]['ParsedText']
+# ------
+def most_common(list):
+    sortedList = sorted((x,i) for i, x in enumerate(list))
+    groups = itertools.groupby(sortedList, key=operator.itemgetter(0))
+    def _auxfun(g):
+        item, iterable = g
+        count = 0
+        min_index = len(list)
+        for _, where in iterable:
+            count += 1
+            min_index = min(min_index, where)
+        return count, -min_index
+    return max(groups, key=_auxfun)[0]
+
+
+def listNames(splitline):
+    names = []
+    for str in splitline:
+        str = str[0:-1]
+        data = st.tag(str.split())
+        for tup in data:
+            if tup[1] == 'PERSON':
+                names.append(tup[0])
+    return names
+
+def oneName(list):
+    combinedNames = [i+' '+j for i,j in zip(list[::2],list[1::2])]
+    return most_common(combinedNames)
+
+manyNames = listNames(recj)
+print(manyNames)
+print(oneName(manyNames))
