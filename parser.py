7e4f38c40f5b75a542ea4fcd6600e46157475fd3
Laura Goon <laura.gooon@gmail.com>
2017-01-07 17:06:35 -0500

    cna find applicant's email address

--- parser.py
+++ parser.py
@@ -1,10 +1,14 @@
 import requests
 import json
+#--- FIND NAME --
 import itertools
 import operator
 from nltk.tag.stanford import StanfordNERTagger
-st = StanfordNERTagger('stanford-ner/english.all.3class.distsim.crf.ser.gz', 'stanford-ner/stanford-ner.jar')
+#--- FIND EMAIL --
+import re
 
+
+# ---------- FROM PDF TO TEXT ----------
 def ocr_space_file(filename, overlay=False, api_key='186247b66c88957', language='eng'):
     """ OCR.space API request with local file.
         Python3.5 - not tested on 2.7
@@ -64,19 +68,18 @@ resume_json = json.loads(test_url)
 
 # print(rec_json['ParsedResults'][0]['ParsedText'])
 # print(resume_json['ParsedResults'][0]['ParsedText'])
+# ---------- END (PDF TO TEXT) ----------
 
-
-
-# look for name
-
-#--- remove newlines
+# ----- splitlines the data -----
 recj = rec_json['ParsedResults'][0]['ParsedText']
-# recj = recj.replace("\r","")
-# recj = recj.replace("\n","")
 recj = recj.splitlines()
-# print(recj)
 resumej = resume_json['ParsedResults'][0]['ParsedText']
-# ------
+resumej = resumej.splitlines()
+# ----- end (splitlines the data) -----
+
+# ---------- FIND NAME OF APPLICANT ----------
+st = StanfordNERTagger('stanford-ner/english.all.3class.distsim.crf.ser.gz', 'stanford-ner/stanford-ner.jar')
+
 def most_common(list):
     sortedList = sorted((x,i) for i, x in enumerate(list))
     groups = itertools.groupby(sortedList, key=operator.itemgetter(0))
@@ -89,7 +92,6 @@ def most_common(list):
             min_index = min(min_index, where)
         return count, -min_index
     return max(groups, key=_auxfun)[0]
-
 
 def listNames(splitline):
     names = []
@@ -105,6 +107,29 @@ def oneName(list):
     combinedNames = [i+' '+j for i,j in zip(list[::2],list[1::2])]
     return most_common(combinedNames)
 
-manyNames = listNames(recj)
-print(manyNames)
-print(oneName(manyNames))
+# manyNames = listNames(resumej)
+# print(oneName(manyNames))
+# ---------- END (FIND NAME OF APPLIANT) ----------
+
+
+# ---------- FIND APPLICANT EMAIL ----------
+regex = re.compile(("([a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`"
+                    "{|}~-]+)*(@|\sat\s)(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?(\.|"
+                    "\sdot\s))+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)"))
+
+def findEmail(splitline):
+    ret = []
+    realRet = []
+    for str in splitline:
+        match = re.findall(r'[\w\.-]+@[\w\.-]+', str)
+        ret.append(match)
+    for item in ret:
+        if item != []:
+            realRet.append(item[0])
+    if len(realRet) == 1:
+        return realRet[0]
+    else:
+        return realRet
+
+# print(findEmail(resumej))
+# ---------- END (FIND APPLICANT EMAIL) ----------
