7d2bd7940c3f5c6dad20bb3a8b0649cdf2ebcf13
nancycao <ncao@stuy.edu>
2017-01-07 23:25:10 -0500

    modified html and app

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 import os
 from flask import Flask, request, redirect, url_for, render_template
-from werkzeug.utils import secure_filename
-
-UPLOAD_FOLDER = '/uploads'
-ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg'])
+import database
+import parser
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route("/") #Makes index show up when the root folder ("/") of the server is accessed by a user
+@app.route("/", methods = ['POST','GET']) #Makes index show up when the root folder ("/") of the server is accessed by a user
 def index():
     return render_template("main.html")
+
+@app.route("/results/", methods = ['POST','GET'])
+def results():
+    link = request.form['link']
+    return render_template("results.html", link=link)
+
+@app.route("/search/", methods = ['POST','GET'])
 
 if __name__ == "__main__": #Ensures that the app will only start if this file is not being imported from somewhere else but rather being accessed directly
     app.run()
