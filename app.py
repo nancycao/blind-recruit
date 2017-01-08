10dcdddd3856dbff1a3c9b8d8e75de8032960ebb
nancycao <ncao@stuy.edu>
2017-01-07 22:40:10 -0500

    cahnged html and app

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask #Imports the Flask class from the flask package that we installed
-app = Flask(__name__) #Creates an instance of this class
+import os
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/") #Makes index show up when the root folder ("/") of the server is accessed by a user
 def index():
-    return "home"
+    return render_template("main.html")
 
 if __name__ == "__main__": #Ensures that the app will only start if this file is not being imported from somewhere else but rather being accessed directly
     app.run()
